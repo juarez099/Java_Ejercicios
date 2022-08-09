60a9e27c45cffd8fddf13ee88f731e66d769b9ed
Luis Juarez <juarezcorona57@gmail.com>
2022-08-09 13:24:49 -0500

    Sumatoria por introduccion de usuario

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
         System.out.println("Bienvenido a Java Luis Juarez");
+
+        System.out.println("Digita un numero: ");
+        double num1=leer.nextDouble();
+        System.out.println("Digita un numero: ");
+        double num2=leer.nextDouble();
+
+        double total= num1+num2;
+        System.out.println("total es = " + total);
+
+
+
     }
 }
