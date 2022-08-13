d574363ca083628a35afadf6ab8ea86a5f103646
Luis Juarez <juarezcorona57@gmail.com>
2022-08-12 22:26:06 -0500

    Condicional ternaria lograda

--- src/Suma.java
+++ src/Suma.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Suma {
     public static void main(String[] args) {
         Scanner leer=new Scanner(System.in);
-
+        String sumfin;
         System.out.println("Digita un numero: ");
         double num1=leer.nextDouble();
         System.out.println("Digita un numero: ");
@@ -10,5 +10,8 @@ public class Suma {
 
         double total= num1+num2;
         System.out.println("total es = " + total);
+
+        sumfin=total >= 30 || total <=50 ? "EL total es mayor a 30 y menor a 50":"Mayor a 50 o menor a 30";
+        System.out.println("sumfin = " + sumfin);
     }
 }
