f3ee48a2d9db28b1d7f23505a7dae7f3b8429677
Luis Juarez <juarezcorona57@gmail.com>
2022-08-15 21:34:18 -0500

    De Fahranheit a Celcius

--- src/GradosFaC.java
+++ src/GradosFaC.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class GradosFaC {
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        
+        double gradoF=0,C=0;
+        System.out.println("Digita los grados en Fahrenheit: ");
+        gradoF=leer.nextDouble();
 
+        C=(gradoF-32)/1.8;
+        System.out.println("Fahrenheit: "+ gradoF +" son " + C + " Celcius");
     }
 }
